ee2112058db0872df010545ab3ce0ae1a92f295d
Peter DeWeese <peter.deweese@gmail.com>
2013-07-09 13:21:15 -0400

    Added rubyish string methods.

--- shared/NSString+rubyish.h
+++ shared/NSString+rubyish.h
@@ -1,109 +1,113 @@
 #import <Foundation/Foundation.h>
 
-// Methods based off of Ruby 2.0.
 // See NSMutableString+rubyish.h for mutation methods.
-// When methods have a mutable and immutable method, the mutable method is named like 'delete', and
-// the immutable version is 'deleted'.
 @interface NSString(rubyish)
-    // not implemented: ::new
+    // Mutable:
+    // not implemented: #capitalize!
+    // not implemented: #chomp!
+    // not implemented: #chop!
+    // not implemented: #clear
+    // not implemented: #delete!
+    // not implemented: #encode!
+    // not implemented: #downcase!
+    // not implemented: #gsub!
+    // not implemented: #insert
+    // not implemented: #lstrip!
+    // not implemented: #next!
+    // not implemented: #prepend
+    // not implemented: #reverse!
+    // not implemented: #rstrip!
+    // not implemented: #scrub!
+    // not implemented: #slice!
+    // not implemented: #squeeze!
+    // not implemented: #strip!
+    // not implemented: #sub!
+    // not implemented: #succ!
+    // not implemented: #swapcase!
+    // not implemented: #tr!
+    // not implemented: #tr_s!
+
+    //::new, Use -copy
     // not implemented: ::try_convert
     // not implemented: #%
     // not implemented: #*
     // not implemented: #+
     // not implemented: #<<
     // not implemented: #<=>
-    // not implemented: #==
+    // #==, Use -isEqual:
     // not implemented: #===
     // not implemented: #=~
     // not implemented: #[]
     // not implemented: #[]=
     // not implemented: #ascii_only?
     // not implemented: #b
-    // not implemented: #bytes
-    // not implemented: #bytesize
+    -(const void *)bytes; // #bytes
+    -(NSUInteger)bytesize; // #bytesize
     // not implemented: #byteslice
     -(NSString*)capitalized; // #capitalize
-    // not implemented: #capitalize!
-    // not implemented: #casecmp
+    -(NSComparisonResult)casecmp:(NSString*)with; // #casecmp
     // not implemented: #center
     // not implemented: #chars
     // not implemented: #chomp
-    // not implemented: #chomp!
     // not implemented: #chop
-    // not implemented: #chop!
-    // not implemented: #chr
-    // not implemented: #clear
+    -(NSString*)chr; // #chr
     // not implemented: #codepoints
     // not implemented: #concat
     // not implemented: #count
     // not implemented: #crypt
     // not implemented: #delete
-    // not implemented: #delete!
     -(NSString*)downcased; // #downcase
-    // not implemented: #downcase!
     // not implemented: #dump
-    // not implemented: #each_byte
-    // not implemented: #each_char
+    -(void)eachByte:(void(^)(char b))block; // #each_byte
+    -(void)eachChar:(void(^)(unichar c))block; // #each_char
     // not implemented: #each_codepoint
-    // not implemented: #each_line
+    -(void)eachLine:(void(^)(NSString *line))block; //#each_line
+    // not implemented: #each_line(separator=$/) {|substr| block }
     -(BOOL)isEmpty; // #empty?
     // not implemented: #encode
-    // not implemented: #encode!
     // not implemented: #encoding
-    // not implemented: #end_with?
+    -(BOOL)endsWith:(NSString*)suffix; // #end_with?
+    -(BOOL)endsWithAny:(NSArray*)suffixes; // #end_with?([suffixes]+)
     // not implemented: #eql?
     // not implemented: #force_encoding
     // not implemented: #getbyte
     // not implemented: #gsub
-    // not implemented: #gsub!
-    // not implemented: #hash
+    // #hash, use -hash.
     // not implemented: #hex
     -(BOOL)includes:(NSString*)string;// #include?
     // not implemented: #index
     // not implemented: #initialize_copy
-    // not implemented: #insert
     // not implemented: #inspect
     // not implemented: #intern
-    // not implemented: #length
+    // #length, Use -length
     // not implemented: #lines
     // not implemented: #ljust
-    // not implemented: #lstrip
-    // not implemented: #lstrip!
+    -(NSString*)lstripped; // #lstrip
     // not implemented: #match
     // not implemented: #next
-    // not implemented: #next!
     // not implemented: #oct
     // not implemented: #ord
     // not implemented: #partition
-    // not implemented: #prepend
     // not implemented: #replace
-    // not implemented: #reverse
-    // not implemented: #reverse!
+    -(NSString*)reversed; // #reverse
     // not implemented: #rindex
     // not implemented: #rjust
     // not implemented: #rpartition
-    // not implemented: #rstrip
-    // not implemented: #rstrip!
+    -(NSString*)rstripped; // #rstrip
     // not implemented: #scan
     // not implemented: #scrub
-    // not implemented: #scrub!
     // not implemented: #setbyte
     -(NSUInteger)size; // #size
     // not implemented: #slice
-    // not implemented: #slice!
     // not implemented: #split
     // not implemented: #squeeze
-    // not implemented: #squeeze!
-    // not implemented: #start_with?
+    -(BOOL)startsWith:(NSString*)prefix;// #start_with?
+    -(BOOL)startsWithAny:(NSArray*)prefixes;// start_with?([prefixes]+)
     -(NSString*)stripped; // #strip
-    // not implemented: #strip!
     // not implemented: #sub
-    // not implemented: #sub!
     // not implemented: #succ
-    // not implemented: #succ!
     // not implemented: #sum
-    // not implemented: #swapcase
-    // not implemented: #swapcase!
+    -(NSString*)swapcased;// #swapcase
     // not implemented: #to_c
     // not implemented: #to_f
     // not implemented: #to_i
@@ -112,9 +116,7 @@
     // not implemented: #to_str
     // not implemented: #to_sym
     // not implemented: #tr
-    // not implemented: #tr!
     // not implemented: #tr_s
-    // not implemented: #tr_s!
     // not implemented: #unpack
     -(NSString*)upcased; // #upcase
     // not implemented: #upcase!
--- shared/NSString+rubyish.m
+++ shared/NSString+rubyish.m
@@ -2,22 +2,122 @@
 
 @implementation NSString(rubyish)
 
+-(const void *)bytes
+{
+    NSData* data = [self dataUsingEncoding:NSUTF8StringEncoding];
+    return data.bytes;
+}
+
+-(NSUInteger)bytesize
+{
+    return [self lengthOfBytesUsingEncoding:NSUTF8StringEncoding];
+}
+
 -(NSString*)capitalized { return self.capitalizedString; }
+
+-(NSComparisonResult)casecmp:(NSString*)with { return [self caseInsensitiveCompare:with]; }
+
+-(NSString*)chr { return self.isEmpty ? nil : [self substringToIndex:1]; }
 
 -(NSString*)downcased { return self.lowercaseString; }
 
+-(void)eachByte:(void(^)(char b))block
+{
+    NSUInteger length = self.bytesize;
+    const char* bytes = self.bytes;
+    for(int i = 0; i<length; i++)
+        block(bytes[i]);
+}
+
+-(void)eachChar:(void(^)(unichar c))block
+{
+    unichar buffer[self.length];
+    [self getCharacters:buffer range:NSMakeRange(0, self.length)];
+    for(int i = 0; i < self.length; ++i)
+        block(buffer[i]);
+}
+
+-(void)eachLine:(void(^)(NSString *line))block
+{
+    [self enumerateLinesUsingBlock:^(NSString *line, BOOL *stop) {
+            block(line);
+        }];
+}
+
 -(BOOL)isEmpty { return !self.length; }
 
--(BOOL)includes:(NSString*)string
+-(BOOL)endsWith:(NSString*)suffix { return [self hasSuffix:suffix]; }
+
+-(BOOL)endsWithAny:(NSArray*)suffixes
 {
-    return [self rangeOfString:string].length;
+    for(NSString* suffix in suffixes)
+        if([self endsWith:suffix])
+            return YES;
+    return NO;
+}
+
+-(BOOL)includes:(NSString*)string { return [self rangeOfString:string].length; }
+
+-(NSString*)lstripped
+{
+    int i;
+    for(i = 0; i < self.length; i++)
+        if(![NSCharacterSet.whitespaceAndNewlineCharacterSet characterIsMember: [self characterAtIndex:i]])
+            break;
+    return [self substringFromIndex: i];
+}
+
+-(NSString*)reversed //http://rosettacode.org/wiki/Reverse_a_string#Objective-C
+{
+    NSInteger l = self.length - 1;
+	NSMutableString *ostr = [NSMutableString stringWithCapacity:self.length];
+	while (l >= 0)
+	{
+		NSRange range = [self rangeOfComposedCharacterSequenceAtIndex:l];
+		[ostr appendString:[self substringWithRange:range]];
+		l -= range.length;
+	}
+	return ostr;
+}
+
+-(NSString*)rstripped
+{
+    NSInteger i;
+    for(i = self.length-1; i >= 0; i--)
+        if(![NSCharacterSet.whitespaceAndNewlineCharacterSet characterIsMember: [self characterAtIndex:i]])
+            break;
+    return [self substringToIndex: i+1];
 }
 
 -(NSUInteger)size { return self.length; }
 
+-(BOOL)startsWith:(NSString*)prefix { return [self hasPrefix:prefix]; }
+
+-(BOOL)startsWithAny:(NSArray*)prefixes
+{
+    for(NSString* prefix in prefixes)
+        if([self startsWith:prefix])
+            return YES;
+    return NO;
+}
+
 -(NSString*)stripped
 {
     return [self stringByTrimmingCharactersInSet:NSCharacterSet.whitespaceAndNewlineCharacterSet];
+}
+
+-(NSString*)swapcased
+{
+    unichar *s = calloc(self.length, sizeof(unichar));
+    [self getCharacters:s];
+    for(int i=0;i<self.length;i++)
+    {
+        if(s[i] >= 'A' && s[i] <= 'Z')
+            s[i] = s[i] + 'a' - 'A';
+        else if(s[i] >= 'a' && s[i] <= 'z')
+            s[i] = s[i] - ('a' - 'A');
+    }
+    return [NSString stringWithCharacters:s length:self.length];
 }
 
 -(NSString*)upcased { return self.uppercaseString; }
