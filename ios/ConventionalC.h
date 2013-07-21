e2725522ab7d53ef2b99bc8c6dad09064bd36cb5
Peter DeWeese <peter.deweese@gmail.com>
2013-07-20 20:38:37 -0400

    Added png image representation convenience method.

--- ios/ConventionalC.h
+++ ios/ConventionalC.h
@@ -1,6 +1,7 @@
 #import "ConventionalC_Shared.h"
 #import "UIBarButtonItem+ConventionalC.h"
 #import "UIDevice+ConventionalC.h"
+#import "UIImage+ConventionalC.h"
 #import "UIToolbar+ConventionalC.h"
 #import "UIView+ConventionalC.h"
 
