abb2f430254cb5277870062fff670ed5ab81b11e
Peter DeWeese <peter.deweese@gmail.com>
2013-09-02 20:06:23 -0400

    Added UITableView categories to main header file.

--- ios/ConventionalC.h
+++ ios/ConventionalC.h
@@ -2,6 +2,7 @@
 #import "UIBarButtonItem+ConventionalC.h"
 #import "UIDevice+ConventionalC.h"
 #import "UIImage+ConventionalC.h"
+#import "UITableView+ConventionalC.h"
 #import "UIToolbar+ConventionalC.h"
 #import "UIView+ConventionalC.h"
 #import "CALayer+XibConfiguration.h"
